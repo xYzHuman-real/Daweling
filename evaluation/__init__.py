c34ad86496a3bcb83f6ecfa9a61ecbc3d879a50a
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-09 07:51:33 +0530

    Export checkpoint evaluation and release pipeline

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -1,10 +1,12 @@
 """Evaluation primitives and suite runners for Daweling model development."""
 
+from .checkpoint_evaluator import evaluate_checkpoint
 from .checkpoint_selection import CheckpointCandidate, CheckpointSelection, select_checkpoint
 from .experiment import ExperimentRecord, load_experiment, save_experiment
 from .history import BenchmarkDelta, ExperimentComparison, compare_experiment_files, compare_experiments
 from .metrics import exact_match, mean_score, perplexity
 from .regression import RegressionReport, compare_scores
+from .release import evaluate_and_select_checkpoints
 from .suites import BenchmarkSpec, EvaluationSuiteReport, run_suite
 
 __all__ = [
@@ -19,6 +21,8 @@ __all__ = [
     "compare_experiment_files",
     "compare_experiments",
     "compare_scores",
+    "evaluate_and_select_checkpoints",
+    "evaluate_checkpoint",
     "exact_match",
     "load_experiment",
     "mean_score",
