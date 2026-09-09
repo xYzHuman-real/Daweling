35595d252fb057711a92e6a0bde0b7d97469e4bc
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-09 11:14:00 +0530

    Export learning engine primitives

--- memory/__init__.py
+++ memory/__init__.py
@@ -1,10 +1,13 @@
 """Memory primitives for Daweling."""
 
 from .experience import ExperienceRecorder, WorkflowExperience
+from .learning import LearningContext, LearningEngine
 from .store import MemoryEntry, MemoryStore
 
 __all__ = [
     "ExperienceRecorder",
+    "LearningContext",
+    "LearningEngine",
     "MemoryEntry",
     "MemoryStore",
     "WorkflowExperience",
