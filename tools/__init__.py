1eb55ef913dfa11cf82c62b697485984bdccd22f
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-09 06:26:39 +0530

    refactor: export capability configuration and HTTP adapters

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,12 +2,18 @@
 
 from .base import BaseTool, ToolResult
 from .code_runner import CodeRunnerTool
+from .config import CapabilityConfig
+from .http_backends import HttpBackendError, HttpSandboxBackend, HttpSearchBackend
 from .registry import ToolRegistry
 from .web_search import WebSearchTool
 
 __all__ = [
     "BaseTool",
+    "CapabilityConfig",
     "CodeRunnerTool",
+    "HttpBackendError",
+    "HttpSandboxBackend",
+    "HttpSearchBackend",
     "ToolRegistry",
     "ToolResult",
     "WebSearchTool",
