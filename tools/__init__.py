43ee2c4d1642eb2feaee3f81dc1cf9b231a16b28
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-09 06:23:31 +0530

    Export web search and code runner tools

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,6 +1,14 @@
 """Tooling primitives and registry for Daweling."""
 
 from .base import BaseTool, ToolResult
+from .code_runner import CodeRunnerTool
 from .registry import ToolRegistry
+from .web_search import WebSearchTool
 
-__all__ = ["BaseTool", "ToolRegistry", "ToolResult"]
+__all__ = [
+    "BaseTool",
+    "CodeRunnerTool",
+    "ToolRegistry",
+    "ToolResult",
+    "WebSearchTool",
+]
