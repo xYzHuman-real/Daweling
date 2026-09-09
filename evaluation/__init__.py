7d05cd6f8976a9f38bfb9a13968e8df2c13e5d3b
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-09 15:06:03 +0530

    Export standard intelligence benchmark suite

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -6,6 +6,7 @@ from .experiment import ExperimentRecord, load_experiment, save_experiment
 from .history import BenchmarkDelta, ExperimentComparison, compare_experiment_files, compare_experiments
 from .instruction import InstructionCase, InstructionReport, InstructionResult, run_instruction_evaluation
 from .intelligence import IntelligenceCase, IntelligenceReport, IntelligenceResult, contains_all, run_intelligence_evaluation
+from .intelligence_suite import CapabilityThreshold, default_cases, run_standard_intelligence_suite
 from .metrics import exact_match, mean_score, perplexity
 from .model_registry import ModelRegistry, ModelRegistryEntry, RegistrySnapshot
 from .model_suite import ModelEvaluationReport, evaluate_model
@@ -18,15 +19,15 @@ from .release_manifest import ReleaseManifest
 from .suites import BenchmarkSpec, EvaluationSuiteReport, run_suite
 
 __all__ = [
-    "BenchmarkDelta", "BenchmarkSpec", "CheckpointCandidate", "CheckpointSelection",
+    "BenchmarkDelta", "BenchmarkSpec", "CapabilityThreshold", "CheckpointCandidate", "CheckpointSelection",
     "EvaluationSuiteReport", "ExperimentComparison", "ExperimentRecord", "InstructionCase",
     "InstructionReport", "InstructionResult", "IntelligenceCase", "IntelligenceReport",
     "IntelligenceResult", "ModelEvaluationReport", "ModelRegistry", "ModelRegistryEntry",
     "ReasoningCase", "ReasoningRegressionReport", "ReasoningReport", "ReasoningResult",
     "RegistrySnapshot", "RegressionReport", "ReleaseManifest", "compare_experiment_files",
-    "compare_experiments", "compare_reasoning_checkpoints", "compare_scores",
-    "contains_all", "evaluate_and_select_checkpoints", "evaluate_checkpoint", "evaluate_model",
+    "compare_experiments", "compare_reasoning_checkpoints", "compare_scores", "contains_all",
+    "default_cases", "evaluate_and_select_checkpoints", "evaluate_checkpoint", "evaluate_model",
     "evaluate_reasoning_checkpoint", "exact_match", "load_experiment", "load_reasoning_checkpoint",
     "mean_score", "perplexity", "run_instruction_evaluation", "run_intelligence_evaluation",
-    "run_reasoning_evaluation", "run_suite", "save_experiment", "select_checkpoint",
+    "run_reasoning_evaluation", "run_standard_intelligence_suite", "run_suite", "save_experiment", "select_checkpoint",
 ]
