ae7684d89d7dd3cfa87a170b3cffe8d28c6394f3
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-09 06:00:33 +0530

    Export local model provider

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 """Model provider abstractions for Daweling."""
 
 from .base import ModelMessage, ModelProvider, ModelResponse
+from .mock import StaticModelProvider
 
-__all__ = ["ModelMessage", "ModelProvider", "ModelResponse"]
+__all__ = ["ModelMessage", "ModelProvider", "ModelResponse", "StaticModelProvider"]
