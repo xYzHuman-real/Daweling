1e77fcbe4681a70d3bdd99167918477cc8d73987
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-09 14:31:59 +0530

    Export planning guidance from memory

--- memory/__init__.py
+++ memory/__init__.py
@@ -1,14 +1,18 @@
 """Memory primitives for Daweling."""
 
+from .context import ContextBundle, ContextEngine
 from .experience import ExperienceRecorder, WorkflowExperience
-from .learning import LearningContext, LearningEngine
+from .learning import LearningContext, LearningEngine, PlanningGuidance
 from .store import MemoryEntry, MemoryStore
 
 __all__ = [
+    "ContextBundle",
+    "ContextEngine",
     "ExperienceRecorder",
     "LearningContext",
     "LearningEngine",
     "MemoryEntry",
     "MemoryStore",
+    "PlanningGuidance",
     "WorkflowExperience",
 ]
