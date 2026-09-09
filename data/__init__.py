0db177c30efc47ee82ebb7e2936ea7751f7f3515
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-09 10:30:12 +0530

    data: export dataset partition APIs

--- data/__init__.py
+++ data/__init__.py
@@ -1,13 +1,20 @@
 """Public data-engine APIs."""
 
+from .loader import DatasetPartitions, load_partitions
 from .manifest import DatasetManifest, canonical_example_hash, sha256_file
 from .prepare import normalize_example, prepare_dataset, validate_example
+from .split import read_jsonl, split_examples, write_jsonl
 
 __all__ = [
     "DatasetManifest",
+    "DatasetPartitions",
     "canonical_example_hash",
+    "load_partitions",
     "normalize_example",
     "prepare_dataset",
+    "read_jsonl",
     "sha256_file",
+    "split_examples",
     "validate_example",
+    "write_jsonl",
 ]
