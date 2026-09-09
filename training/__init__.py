10b5130201db56b737cdad1c4f8736d9e08a27be
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-09 10:22:32 +0530

    Export end-to-end training release pipeline

--- training/__init__.py
+++ training/__init__.py
@@ -1,6 +1,16 @@
 """Training package public APIs."""
 
 from .experiment import TrainingRunManifest, make_run_id, sha256_file
-from .train import make_examples, train, validation_loss
+from .release_pipeline import train_evaluate_release
+from .train import make_batch, make_examples, train, validation_loss
 
-__all__ = ["TrainingRunManifest", "make_examples", "make_run_id", "sha256_file", "train", "validation_loss"]
+__all__ = [
+    "TrainingRunManifest",
+    "make_batch",
+    "make_examples",
+    "make_run_id",
+    "sha256_file",
+    "train",
+    "train_evaluate_release",
+    "validation_loss",
+]
