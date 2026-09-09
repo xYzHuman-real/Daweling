00fbbd79ffbc7145d358f29c6058145338011cce
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-09 10:57:57 +0530

    Export unified model evaluation APIs

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -6,7 +6,10 @@ from .experiment import ExperimentRecord, load_experiment, save_experiment
 from .history import BenchmarkDelta, ExperimentComparison, compare_experiment_files, compare_experiments
 from .instruction import InstructionCase, InstructionReport, InstructionResult, run_instruction_evaluation
 from .metrics import exact_match, mean_score, perplexity
+from .model_suite import ModelEvaluationReport, evaluate_model
 from .reasoning import ReasoningCase, ReasoningReport, ReasoningResult, run_reasoning_evaluation
+from .reasoning_checkpoint import evaluate_reasoning_checkpoint, load_reasoning_checkpoint
+from .reasoning_regression import ReasoningRegressionReport, compare_reasoning_checkpoints
 from .regression import RegressionReport, compare_scores
 from .release import evaluate_and_select_checkpoints
 from .release_manifest import ReleaseManifest
@@ -23,18 +26,24 @@ __all__ = [
     "InstructionCase",
     "InstructionReport",
     "InstructionResult",
+    "ModelEvaluationReport",
     "ReasoningCase",
+    "ReasoningRegressionReport",
     "ReasoningReport",
     "ReasoningResult",
     "RegressionReport",
     "ReleaseManifest",
     "compare_experiment_files",
     "compare_experiments",
+    "compare_reasoning_checkpoints",
     "compare_scores",
     "evaluate_and_select_checkpoints",
     "evaluate_checkpoint",
+    "evaluate_model",
+    "evaluate_reasoning_checkpoint",
     "exact_match",
     "load_experiment",
+    "load_reasoning_checkpoint",
     "mean_score",
     "perplexity",
     "run_instruction_evaluation",
