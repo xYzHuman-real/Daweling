becb64c520bcd5d91972521af854e5a70549c0ea
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-09 15:11:23 +0530

    Export curriculum mixing APIs

--- training/__init__.py
+++ training/__init__.py
@@ -1,6 +1,7 @@
 """Training package public APIs."""
 
 from .curriculum import CurriculumBatch, CurriculumExample, CurriculumScheduler, CurriculumStage
+from .curriculum_mixer import CurriculumMix, CurriculumMixer, MixedExample
 from .experiment import TrainingRunManifest, make_run_id, sha256_file
 from .release_pipeline import train_evaluate_release
 from .train import make_batch, make_examples, train, validation_loss
@@ -8,8 +9,11 @@ from .train import make_batch, make_examples, train, validation_loss
 __all__ = [
     "CurriculumBatch",
     "CurriculumExample",
+    "CurriculumMix",
+    "CurriculumMixer",
     "CurriculumScheduler",
     "CurriculumStage",
+    "MixedExample",
     "TrainingRunManifest",
     "make_batch",
     "make_examples",
