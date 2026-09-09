c963dad249ecb4df357713aaa2605da44a1ac29f
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-09 15:08:47 +0530

    Export model capability curriculum

--- training/__init__.py
+++ training/__init__.py
@@ -1,10 +1,15 @@
 """Training package public APIs."""
 
+from .curriculum import CurriculumBatch, CurriculumExample, CurriculumScheduler, CurriculumStage
 from .experiment import TrainingRunManifest, make_run_id, sha256_file
 from .release_pipeline import train_evaluate_release
 from .train import make_batch, make_examples, train, validation_loss
 
 __all__ = [
+    "CurriculumBatch",
+    "CurriculumExample",
+    "CurriculumScheduler",
+    "CurriculumStage",
     "TrainingRunManifest",
     "make_batch",
     "make_examples",
